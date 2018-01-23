22a9c89dd2bbd6948c1bccd5df7f58ff0fa9fe31
Scott Umsted <scottumsted@gmail.com>
2018-01-23 12:29:06 -0600

    using hall to count spinner revolutions

--- hall/hall.ino
+++ hall/hall.ino
@@ -2,12 +2,15 @@
 //values can be quite low.
 //Brian Degger / @sctv
 
-#define EMA_A .1
-
+#define EMA_A .8
+#define INACTIVE 37
+#define THRESHOLD 34
 int emaS;
-
+int ticks = 0;
 int val = 0;
 int lastVal = 0;
+bool in_tick = false;
+
 void setup() {
   Serial.begin(9600);
   val = hallRead();
@@ -18,13 +21,17 @@ void loop() {
   // put your main code here, to run repeatedly:
   val = hallRead();
   emaS = lowPassFilter(val);
-  int delta = emaS - lastVal;
-  lastVal = emaS;
-  int tick = delta < -10 ? 30: 36;
-  Serial.println(delta);
-//  Serial.print(", ");
-//  Serial.println(emaS);
-  delay(100);
+  if(!in_tick && emaS < THRESHOLD){
+    ticks++;
+    in_tick = true;
+  } else if(emaS > THRESHOLD){
+    in_tick = false;
+  }
+
+  Serial.print(in_tick+20);
+  Serial.print(", ");
+  Serial.println(emaS);
+  delay(20);
 }
 
 int lowPassFilter(int val){
