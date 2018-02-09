051b1c7d5e6799c4bb237c42d908a1b2ed1b655f
Scott Umsted <scottumsted@gmail.com>
2018-02-09 07:34:20 -0600

    adding io ada to touch

---
+++ touch/requests.cpp
@@ -0,0 +1,179 @@
+#include "Requests.h"
+
+
+Requests::Requests(){
+//    readEepromConfig();
+    establishWifiConnection();
+}
+
+Requests::Requests(ConfigurationUnion *newConfig){
+//    writeNewConfig(newConfig);
+//    readEepromConfig();
+    config = newConfig;
+    establishWifiConnection();
+}
+
+int Requests::getRequest(char *payload){
+    WiFiClient client;
+    char buffer[500];
+    char *action = "GET";
+    sprintf(buffer, "%s %s%s %s/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n",
+            action, config->configuration.gatewayPath, payload, config->configuration.gatewayProtocol,
+            config->configuration.model, config->configuration.deviceId);
+    if(!client.connect(config->configuration.gatewayHost, config->configuration.gatewayPort)){
+        Serial.println("client connection failed");
+        return NULL;
+    }
+    client.print(buffer);
+    unsigned long sendTime = millis();
+    while(client.available() == 0){
+        if(millis() - sendTime > 5000){
+            Serial.println("client timeout");
+            return NULL;
+        }
+    }
+    while(client.available()){
+        //read data
+        String response = client.readStringUntil('\r');
+        Serial.println(response);
+    }
+    client.stop();
+    Serial.println("connection closed");
+}
+
+int Requests::postRequest(char *payload){
+    WiFiClient client;
+    char buffer[500];
+    char *action = "POST";
+    sprintf(buffer, "%s %s %s/1.1\r\nHost: %s%s\r\nConnection: close\r\nContent-length: %d\r\n\r\n%s\r\n",
+            action, config->configuration.gatewayPath, config->configuration.gatewayProtocol,
+            config->configuration.model, config->configuration.deviceId, strlen(payload), payload);
+    if(!client.connect(config->configuration.gatewayHost, config->configuration.gatewayPort)){
+        Serial.println("client connection failed");
+        return NULL;
+    }
+    client.print(buffer);
+    unsigned long sendTime = millis();
+    while(client.available() == 0){
+        if(millis() - sendTime > 5000){
+            Serial.println("client timeout");
+            return NULL;
+        }
+    }
+    while(client.available()){
+        //read data
+        String response = client.readStringUntil('\r');
+        Serial.println(response);
+    }
+    client.stop();
+    Serial.println("connection closed");
+}
+
+
+int Requests::postAdaIo(char *user, char *feed, int value){
+    HTTPClient http;
+    char payload[1000];
+    char url[1000];
+    char *payloadPattern = "{\"value\":%d}";
+    char *urlPattern = "https://io.adafruit.com/api/v2/%s/feeds/%s/data";
+    int code;
+
+    sprintf(payload, payloadPattern, value);
+    sprintf(url, urlPattern, user, feed);
+
+    http.begin(url, rootCa);
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("X-AIO-Key", "c8d50f2a58754014abe65dee32b78655");
+    code = http.POST(payload);
+
+    if(code > 0){
+        String response = http.getString();
+        Serial.println(code);
+        Serial.println(response);
+    }else{
+        Serial.print("problem posting to adaio: ");
+        Serial.println(code);
+        Serial.print("url: ");
+        Serial.println(url);
+        Serial.print("payload: ");
+        Serial.println(payload);
+    }
+    http.end();
+}
+
+
+void Requests::establishWifiConnection(){
+    wifiConnected = 0;
+    byte tries=0;
+    Serial.print("establishwificonnection: ");
+    Serial.print(config->configuration.wifiSsid);
+    Serial.print(", ");
+    Serial.print(config->configuration.wifiPassword);
+    WiFi.begin(config->configuration.wifiSsid, config->configuration.wifiPassword);
+    while(WiFi.status() != WL_CONNECTED){
+        Serial.print("attempting to connect, try: ");
+        Serial.println(tries);
+        tries++;
+        if(tries > 10){
+            Serial.print("failed to establish connection to wifi: ");
+            Serial.println(config->configuration.wifiSsid);
+        }
+        delay(500);
+    }
+    Serial.print("connected to wifi with IP address: ");
+    localIp = WiFi.localIP();
+    wifiConnected = 1;
+}
+
+//void Requests::readEepromConfig(){
+//    int i = 0;
+//    memset(&config, '\0', sizeof(config));
+//    for(i=0;i<sizeof(config);i++){
+//        config.bytes[i] = EEPROM.read(i);
+//    }
+//    Serial.println("eeprom configuration read");
+//    printConfig("eeprom", &config);
+//}
+//
+//void Requests::writeNewConfig(ConfigurationUnion *newConfig){
+//    int i=0;
+//    Serial.println("writing new config to eeprom");
+//    for(i=0;i<sizeof(*newConfig);i++){
+//        EEPROM.write(i, newConfig->bytes[i]);
+//    }
+//    Serial.println("write complete");
+//    readEepromConfig();
+//    Serial.println("confirmed eeprom config");
+//    printConfig("eeprom", &config);
+//}
+
+void Requests::printConfig(char *label, ConfigurationUnion *config){
+    Serial.print("\nConfiguration: ");
+    Serial.println(label);
+    Serial.print("serial: ");
+    Serial.println(config->configuration.serial);
+    Serial.print("deviceId: ");
+    Serial.println(config->configuration.deviceId);
+    Serial.print("model: ");
+    Serial.println(config->configuration.model);
+    Serial.print("firmware: ");
+    Serial.println(config->configuration.firmware);
+    Serial.print("location: ");
+    Serial.println(config->configuration.location);
+    Serial.print("gatewayProtocol: ");
+    Serial.println(config->configuration.gatewayProtocol);
+    Serial.print("gatewayHost: ");
+    Serial.println(config->configuration.gatewayHost);
+    Serial.print("gatewayPort: ");
+    Serial.println(config->configuration.gatewayPort);
+    Serial.print("gatewayPath: ");
+    Serial.println(config->configuration.gatewayPath);
+    Serial.print("State 0: ");
+    Serial.println(config->configuration.state[0]);
+    Serial.print("State 1: ");
+    Serial.println(config->configuration.state[1]);
+    Serial.print("State 2: ");
+    Serial.println(config->configuration.state[2]);
+    Serial.print("State 3: ");
+    Serial.println(config->configuration.state[3]);
+}
--- touch/touch.ino
+++ touch/touch.ino
@@ -1,10 +1,16 @@
-
+#include "requests.h"
 #define RED_LIGHT 27
 #define YELLOW_LIGHT 25
 #define GREEN_LIGHT 33
 #define EMA_A .125
+#define LED_BLINK_DELAY 50
 
 int emaS;
+long current = 0;
+long last = 0;
+long total = 0;
+long start = 0;
+Requests *r;
 
 void setup(){
     pinMode(RED_LIGHT, OUTPUT);
@@ -12,31 +18,102 @@ void setup(){
     pinMode(GREEN_LIGHT, OUTPUT);
     Serial.begin(9600);
     emaS = touchRead(T4);
+
+    ConfigurationUnion *pcu = defaultConfig();
+    r = new Requests(pcu);
+    if(r->wifiConnected){
+        blink(GREEN_LIGHT);
+        blink(GREEN_LIGHT);
+    } else {
+        blink(RED_LIGHT);
+        blink(RED_LIGHT);
+    }
 }
 
+// 5% of last 30
 void loop(){
     int touchiness = touchRead(T4);
     int filtered = lowPassFilter(touchiness);
-    Serial.print(touchiness);
-    Serial.print(", ");
-    Serial.println(filtered);
     delay(50);
-    if(filtered<20){
-        digitalWrite(RED_LIGHT, 0);
-        digitalWrite(YELLOW_LIGHT, 0);
-        digitalWrite(GREEN_LIGHT, 1);
-    } else if(filtered > 40){
+    if(filtered<15){
         digitalWrite(RED_LIGHT, 1);
         digitalWrite(YELLOW_LIGHT, 0);
         digitalWrite(GREEN_LIGHT, 0);
+        startContact();
+    } else if(filtered > 45){
+        digitalWrite(RED_LIGHT, 0);
+        digitalWrite(YELLOW_LIGHT, 0);
+        digitalWrite(GREEN_LIGHT, 1);
+        endContact();
     } else {
         digitalWrite(RED_LIGHT, 0);
         digitalWrite(YELLOW_LIGHT, 1);
         digitalWrite(GREEN_LIGHT, 0);
+        endContact();
     }
+
+    current = millis();
+    if((current - last) > 15000 ){
+        r->postAdaIo("scottumsted", "simon.contact", checkContact());
+        last = current;
+    }
+    Serial.print(start);
+    Serial.print(", ");
+    Serial.println(total);
+}
+
+void startContact(){
+    if(start == 0){
+        start = millis();
+    }
+}
+
+void endContact(){
+    if(start > 0){
+        long end = millis();
+        total += end - start;
+        start = 0;
+    }
+}
+
+int checkContact(){
+    int result = 0;
+    endContact();
+    result = total / 1000;
+    total = 0;
+    return result;
 }
 
 int lowPassFilter(int touchiness){
     emaS = (EMA_A*touchiness) + ((1-EMA_A)*emaS);
     return emaS;
+}
+
+void blink(byte pin){
+    digitalWrite(pin, 1);
+    delay(LED_BLINK_DELAY);
+    digitalWrite(pin, 0);
+    delay(LED_BLINK_DELAY/4);
+}
+
+ConfigurationUnion *defaultConfig(){
+    ConfigurationUnion *cu = new ConfigurationUnion;
+    memset(cu, '\0', sizeof(cu));
+    strcpy(cu->configuration.serial, "TC001");
+    strcpy(cu->configuration.deviceId, "TOUCH01");
+    strcpy(cu->configuration.model, "ESP32");
+    strcpy(cu->configuration.firmware, "1.0");
+    strcpy(cu->configuration.wifiSsid, "u1002");
+    strcpy(cu->configuration.wifiPassword, "e4fac6c7");
+    strcpy(cu->configuration.location, "Home");
+    strcpy(cu->configuration.gatewayProtocol,"https");
+    strcpy(cu->configuration.gatewayHost,"io.adafruit.com");
+    cu->configuration.gatewayPort = 443;
+    strcpy(cu->configuration.gatewayPath,"/users");
+    strcpy(cu->configuration.state[1],"1");
+    strcpy(cu->configuration.state[2],"2");
+    strcpy(cu->configuration.state[3],"3");
+    Serial.println("default config initialized");
+//    r->printConfig("default", &cu);
+    return cu;
 }
