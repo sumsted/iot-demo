b33d7519317f08bd9f93f6c6eacf8a22b5f934ea
Scott Umsted <scottumsted@gmail.com>
2018-02-19 07:05:43 -0600

    adding debug as setting

--- iotconnector/app.py
+++ iotconnector/app.py
@@ -2,13 +2,14 @@ from bottle import TEMPLATE_PATH, run, debug
 
 from settings import Settings
 
-debug(True)
 
 settings = Settings()
+
+debug(settings.WEB['DEBUG'])
 
 TEMPLATE_PATH.insert(0, settings.WEB['TEMPLATE_FOLDER'])
 
 import routes
 
 if __name__ == '__main__':
-    run(host=settings.WEB['HOST'], port=settings.WEB['PORT'], debug=True)
+    run(host=settings.WEB['HOST'], port=settings.WEB['PORT'], debug=settings.WEB['DEBUG'])
