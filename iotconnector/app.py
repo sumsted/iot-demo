9e349606683e5228cfb8fd93b091c2f2b1878c4a
Scott Umsted <scottumsted@gmail.com>
2018-02-19 06:39:21 -0600

    forgot to adjust import

--- iotconnector/app.py
+++ iotconnector/app.py
@@ -6,7 +6,7 @@ settings = Settings()
 
 TEMPLATE_PATH.insert(0, settings.WEB['TEMPLATE_FOLDER'])
 
-import iotconnector.routes
+import routes
 
 if __name__ == '__main__':
     run(host=settings.WEB['HOST'], port=settings.WEB['PORT'], debug=True)
